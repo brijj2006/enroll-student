95f43612cb53f2436ecb0edb4976406eeb199cda
Brijendra Singh <brijj2006@gmail.com>
2021-11-06 02:21:12 +0800

    project setup

---
+++ src/test/java/com/school/project/enrollstudent/EnrollStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.project.enrollstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
